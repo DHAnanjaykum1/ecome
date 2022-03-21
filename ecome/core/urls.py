f217136dc971703af069624e9d35d47a31b78230
DHAnanjaykum1 <89680668+DHAnanjaykum1@users.noreply.github.com>
2022-03-21 18:51:31 +0530

    cd

--- ecome/core/urls.py
+++ ecome/core/urls.py
@@ -7,4 +7,6 @@ app_name = "core"
 urlpatterns = [
      path("",HomeView.as_view(),name="homepage"),
      path("products/<slug>/",ItemDetailsView.as_view(),name="item"),
+     path("order-summary/",OrderSummaryVIew.as_view(),name="order-summary"),
+     path("add-to-cart/<slug>/",AddToCart.as_view(),name="add-to-cart"),
 ]
